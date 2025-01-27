80d5d8ef5b3908c1d07c566437c9fed4f5897e51
Pavan9567 <pavank9567@gmail.com>
2025-01-27 16:51:35 +0530

    Home Page

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const NavigationBar = () => {
         <Link to="/" className="text-2xl font-bold">
           <span className="text-orange-400">Register</span>Karo
         </Link>
-        <button className="block md:hidden focus:outline-none" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+        <button type="button" className="block md:hidden focus:outline-none" onClick={() => setIsMenuOpen(!isMenuOpen)}>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"/>
           </svg>
